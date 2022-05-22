15ce4bebedd060fcab8ae5e7e992e6bac481a42b
Ilia Shapovalov <greateman@mail.ru>
2022-05-22 20:24:57 +0300

    Created navigation link between view's.

---
+++ hackerNews.iOSBootcamp/View/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  hackerNews.iOSBootcamp
+//
+//  Created by Илья Шаповалов on 22.05.2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
